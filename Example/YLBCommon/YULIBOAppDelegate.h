390de997772603dace5b4ad3774360ce9eddc581
yulibo <564885081@qq.com>
2020-03-19 10:45:29 +0800

    Initial commit

---
+++ Example/YLBCommon/YULIBOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YULIBOAppDelegate.h
+//  YLBCommon
+//
+//  Created by ProBobo on 03/19/2020.
+//  Copyright (c) 2020 ProBobo. All rights reserved.
+//
+
+@import UIKit;
+
+@interface YULIBOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
