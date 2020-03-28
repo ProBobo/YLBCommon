3713155744d311c4fbba9dc1426dac37b7ab22c5
yulibo <564885081@qq.com>
2020-03-28 21:37:01 +0800

    修改注释

--- YLBCommon/Classes/Manager/YLBBundleManager.h
+++ YLBCommon/Classes/Manager/YLBBundleManager.h
@@ -16,7 +16,7 @@ NS_ASSUME_NONNULL_BEGIN
 
  注册Pod编译后产生的Bundle路径
  
- @param aClass 当前的类，传[self class即可
+ @param aClass 当前的类，传[self class]即可
  @param podName 编译后Pod库产生的bundle名称（对应Pod库的名称），例如Pod的名称为YLBResource，对应的podName名称就是YLBResource
  */
 - (NSBundle *)ylb_rigisterBundleForClass:(Class)aClass URLForResource:(NSString *)podName;
