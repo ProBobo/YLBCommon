c176fa5bad9eb7d4540db2cd677b613e8bec403e
yulibo <564885081@qq.com>
2020-03-29 10:57:41 +0800

    修改shell

--- gitadd.sh
+++ gitadd.sh
@@ -1,13 +1,4 @@
 msg=$1
-if [ -n "$msg" ]; then
-   git add .
-   git commit -m"${msg}"
-   git pull
-   echo -e "\033[32m 完成add、commit、pull操作，别忘了push \033[0m"
-   gitpush1
-else
-    echo -e "\033[31m 请添加注释再来一遍 \033[0m"
-fi
 
 gitpush1() {
 	if [ -z "$(git status -s)" ];then
@@ -17,3 +8,13 @@ gitpush1() {
 		git status
 	fi
 }
+
+if [ -n "$msg" ]; then
+   git add .
+   git commit -m"${msg}"
+   git pull
+   echo -e "\033[32m 完成add、commit、pull操作，别忘了push \033[0m"
+   gitpush1
+else
+    echo -e "\033[31m 请添加注释再来一遍 \033[0m"
+fi
