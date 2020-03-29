d14bcb3164e55836322203034777195132b3b445
yulibo <564885081@qq.com>
2020-03-29 09:52:10 +0800

    修改shell脚本

--- gitadd.sh
+++ gitadd.sh
@@ -4,8 +4,8 @@ if [ -n "$msg" ]; then
    git commit -m"${msg}"
    git pull
    git status
-   echo "完成add、commit、pull操作，别忘了push"
+   echo -e "\033[32m 完成add、commit、pull操作，别忘了push \033[0m"
 else
-    echo "请添加注释再来一遍"
+    echo -e "\033[31m 请添加注释再来一遍 \033[0m"
 fi
 
