ff29ad75ca28bc0ab94848cbdcb78f752ebe714d
yulibo <564885081@qq.com>
2020-03-29 10:55:50 +0800

    修改shell

--- gitadd.sh
+++ gitadd.sh
@@ -3,9 +3,17 @@ if [ -n "$msg" ]; then
    git add .
    git commit -m"${msg}"
    git pull
-   git status
    echo -e "\033[32m 完成add、commit、pull操作，别忘了push \033[0m"
+   gitpush1
 else
     echo -e "\033[31m 请添加注释再来一遍 \033[0m"
 fi
 
+gitpush1() {
+	if [ -z "$(git status -s)" ];then
+		git push origin master
+	else
+		echo 'modified/untracked'
+		git status
+	fi
+}
