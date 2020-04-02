a4965f1046e82f6d56962cbdb23877389c0ae611
yulibo <564885081@qq.com>
2020-04-02 11:53:43 +0800

    添加TableView

---
+++ Example/YLBCommon/Demo/TableView/YULIBOTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  YULIBOTableViewCell.h
+//  YLBCommon_Example
+//
+//  Created by yulibo on 2020/4/2.
+//  Copyright © 2020 余礼钵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString * const kYULIBOTableViewCell;
+
+@interface YULIBOTableViewCell : UITableViewCell
++ (instancetype)getWithTableView:(UITableView *)tableView;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/YLBCommon/Demo/TableView/YULIBOTableViewCell.m
@@ -0,0 +1,38 @@
+//
+//  YULIBOTableViewCell.m
+//  YLBCommon_Example
+//
+//  Created by yulibo on 2020/4/2.
+//  Copyright © 2020 余礼钵. All rights reserved.
+//
+
+#import "YULIBOTableViewCell.h"
+
+@interface YULIBOTableViewCell ()
+
+@end
+
+NSString * const kYULIBOTableViewCell = @"YULIBOTableViewCell";
+
+@implementation YULIBOTableViewCell
+
++ (instancetype)getWithTableView:(UITableView *)tableView {
+    id cell = [tableView dequeueReusableCellWithIdentifier:kYULIBOTableViewCell];
+    if (!cell) {
+        cell = [[[NSBundle mainBundle] loadNibNamed:NSStringFromClass([self class]) owner:nil options:nil] lastObject];
+    }
+    return cell;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
