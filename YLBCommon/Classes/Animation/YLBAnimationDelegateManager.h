cc0298d96aa28e38d521f33f5bffc4a5dca47208
yulibo <libo.yu@quvideo.com>
2020-10-09 16:59:56 +0800

    添加注释

--- YLBCommon/Classes/Animation/YLBAnimationDelegateManager.h
+++ YLBCommon/Classes/Animation/YLBAnimationDelegateManager.h
@@ -17,6 +17,18 @@ NS_ASSUME_NONNULL_BEGIN
 
 @end
 
+/**
+ iOS核心动画内存泄漏CAKeyframeAnimation解决方案:
+ 1、引入代理
+ YLBAnimationWeakDelegate
+ 
+ 2、创建对应实例
+ CAKeyframeAnimation *moveAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+ YLBAnimationDelegateManager * manager = [YLBAnimationDelegateManager new]; //创建实例
+ manager.delegate = self; //进行弱引用
+ moveAnimation.delegate = manager; //进行强引用
+*/
+
 @interface YLBAnimationDelegateManager : NSObject<CAAnimationDelegate>
 @property (weak, nonatomic) id<YLBAnimationWeakDelegate> delegate;
 
